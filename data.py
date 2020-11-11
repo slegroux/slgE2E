0947d657d08f0ce3a5d3ab05f3e9f79832d2ca62
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-11-11 04:26:04 +0000

    pad sequences

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import torchaudio
+import torch
 from torch.utils.data import Dataset
 import torch.nn as nn
 import string
@@ -7,11 +8,13 @@ from IPython import embed
 
 LIBRI_DIR = "/home/syl20/data/en/librispeech"
 
+
 class CharMap:
     def __init__(self):
         m = {}
         m["'"] = 0
         m['<SPACE>'] = 1
+        m['<PAD>'] = 28
         counter = 2
         for letter in string.ascii_lowercase:
             m[letter] = counter
@@ -26,7 +29,7 @@ class CharMap:
     @property
     def map(self)->Dict[str,int]:
         return(self._map)
-    
+
     @property
     def pam(self)->Dict[int,str]:
         return(self._pam)
@@ -54,7 +57,7 @@ class Data(object):
         self.train_ds, self.test_ds = None, None
         self.train_tf, self.test_tf = None, None
 
-    def get_libri_ds(self, data_dir:str=LIBRI_DIR)->(Dataset, Dataset):
+    def get_libri_ds(self, data_dir:str=LIBRI_DIR)->Tuple[Dataset, Dataset]:
         self.train_ds = torchaudio.datasets.LIBRISPEECH(data_dir, url="train-clean-100", download=False)
         self.test_ds = torchaudio.datasets.LIBRISPEECH(data_dir, url="test-clean", download=False)
         return(self.train_ds, self.test_ds)
@@ -65,16 +68,32 @@ class Data(object):
             torchaudio.transforms.FrequencyMasking(freq_mask_param=15),
             torchaudio.transforms.TimeMasking(time_mask_param=35)
         )
-        self._test_tf = torchaudio.transforms.MelSpectrogram()
+        self.test_tf = torchaudio.transforms.MelSpectrogram()
     
     def data_prep(self):
         self.init_tf()
+        cm = CharMap()
         specgrams_train, specgrams_test = [], []
         labels_train, labels_test = [], []
         for (waveform, sample_rate, utterance, speaker_id, chapter_id, utterance_id) in self.train_ds:
-            spec = self.train_tf(waveform).squeeze(0).transpose(0,1)
-            label = self.
-            specgrams_train.append(spec)
+            # waveform: (1, n_samples)
+            # spec = (1, n_bins, n_frames)
+            # dim 1 is useless =>squeeze(0) + padding requires padding dim to be first => transpose(0,1)
+            specgrams_train.append(self.train_tf(waveform).squeeze(0).transpose(0,1))
+            labels_train.append(torch.Tensor(cm.text2int(utterance)))
+        
+        for (waveform, sample_rate, utterance, speaker_id, chapter_id, utterance_id) in self.test_ds:
+            # waveform: (1, n_samples)
+            # spec = (1, n_bins, n_frames)
+            specgrams_test.append(self.test_tf(waveform).squeeze(0).transpose(0,1))
+            labels_test.append(torch.Tensor(cm.text2int(utterance)))
 
-        return(specgrams)
+        # pad
+        spec_train = nn.utils.rnn.pad_sequence(specgrams_train, batch_first=True).transpose(1,2)
+        text_train = nn.utils.rnn.pad_sequence(labels_train, batch_first=True, padding_value=cm.map['<PAD>'])
+        spec_test = nn.utils.rnn.pad_sequence(specgrams_test, batch_first=True).transpose(1,2)
+        text_test = nn.utils.rnn.pad_sequence(labels_test, batch_first=True, padding_value=cm.map['<PAD>'])
+
+        
+        return(spec_train, text_train, spec_test, text_test)
 
