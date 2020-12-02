6bd34f685a74facdc86393b1e7bd15421dd865fc
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2020-12-02 00:31:53 +0000

    check dataset and collate dims

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ from torchaudio.datasets import LIBRISPEECH
 import torch.nn as nn
 import string
 from typing import List, Set, Dict, Tuple, Optional, Callable
-from torch import Tensor
+from torch import Tensor, IntTensor
 from IPython import embed
 
 LIBRI_DIR = "/home/syl20/data/en/librispeech"
@@ -23,6 +23,7 @@ class CharacterTokenizer:
         m["'"] = 0
         m['<SPACE>'] = 1
         m['<PAD>'] = 28
+        # TODO(slg): <EOS>?
         counter = 2
         for letter in string.ascii_lowercase:
             m[letter] = counter
@@ -71,19 +72,30 @@ class LibriDataset(LIBRISPEECH):
             super().__getitem__(n)
         if self.transform:
             waveform = self.transform(waveform)
-
-        return(waveform, sample_rate, utterance, torch.Tensor(self.tokenizer.text2int(utterance)), speaker_id, chapter_id, utterance_id)
+        spec_len = waveform.shape[-1] #// 2
+        label = IntTensor(self.tokenizer.text2int(utterance))
+        label_len = len(label)
+        return(waveform, label, spec_len, label_len)
+        # return(waveform, sample_rate, utterance, IntTensor(self.tokenizer.text2int(utterance)), speaker_id, chapter_id, utterance_id)
 
     def collate(self, batch):
         # batch: (feature, sample_rate, utterance, tokens, speaker_id, chapter_id, utterance_id)
         # features: (channel * n_bin * n_frames)
-        features_len = [ item[0][0].shape[1] for item in batch ] # waveform first channel exract dim(n_frames)
+        # feature:item[0], utterance: item[2], tokens: item[3]
         # padding requires padding dim (n_frames) to be first => transpose(0,2)
-        features = [ item[0].transpose(0,2) for item in batch ]
-        labels_len = [len(item[3]) for item in batch]
-        labels = [ item[3] for item in batch ]
+        features = [ item[0].squeeze(0).transpose(0,1) for item in batch ]
+        # print("features shape", [x.shape for x in features])
+        features_len = [ item[2] for item in batch ] # waveform first channel exract dim(n_frames)
+        # print("max", max(features_len))
+        labels = [ item[1] for item in batch ]
+        labels_len = [item[3] for item in batch]
+        # print("max", max(labels_len))
+        # print("labels shape", [label.shape for label in labels])
         labels = nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=self.tokenizer.map['<PAD>'])
+        # print("padded labels ", [x.shape for x in labels])
         # (batch,channel,n_bins,n_frames)
-        features = nn.utils.rnn.pad_sequence(features, batch_first=True, padding_value=0.0).transpose(1,3)
+        features = nn.utils.rnn.pad_sequence(features, batch_first=True, padding_value=0.0).unsqueeze(1).transpose(2,3)
+        # print("padded feats", [x.shape for x in features])
 
-        return([features, labels, features_len, labels_len])
+        return(features, labels, features_len, labels_len)
+
